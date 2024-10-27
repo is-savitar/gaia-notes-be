b3f1348a9ce33811984f645e3acaf3c607b31bf4
is-savitar <savitar746233@gmail.com>
2024-10-27 17:51:38 +0100

    fix: revamped routes

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -7,10 +7,10 @@ import type Route4 from "../routes/auth/login/index";
 import type Route5 from "../routes/auth/refresh-token/index";
 
 declare global {
-    export type EdenRoutes = ElysiaWithBaseUrl<"/api/v1/", typeof Route0>
-              & ElysiaWithBaseUrl<"/api/v1/profile", typeof Route1>
-              & ElysiaWithBaseUrl<"/api/v1/auth/signup", typeof Route2>
-              & ElysiaWithBaseUrl<"/api/v1/auth/logout", typeof Route3>
-              & ElysiaWithBaseUrl<"/api/v1/auth/login", typeof Route4>
-              & ElysiaWithBaseUrl<"/api/v1/auth/refresh-token", typeof Route5>
+    export type EdenRoutes = ElysiaWithBaseUrl<"/api/v1", typeof Route0>
+              & ElysiaWithBaseUrl<"/api/v1profile", typeof Route1>
+              & ElysiaWithBaseUrl<"/api/v1auth/signup", typeof Route2>
+              & ElysiaWithBaseUrl<"/api/v1auth/logout", typeof Route3>
+              & ElysiaWithBaseUrl<"/api/v1auth/login", typeof Route4>
+              & ElysiaWithBaseUrl<"/api/v1auth/refresh-token", typeof Route5>
 }
